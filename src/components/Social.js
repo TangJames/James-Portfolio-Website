7700e87e3aef36134b1dfbea42e1725cdbd45ae9
James Tang <ulrichyu@outie.net>
2017-11-28 22:37:21 -0600

    added email

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,7 @@ class Social extends Component {
     return (
 			<div className="Social">
 				<ul>
-						<a href="http://github.com/tangjames" target="_blank"><p>Github</p></a> / <a href="https://www.linkedin.com/in/tang-james/" target="_blank"><p>Linkedin</p></a> 
+						<a href="http://github.com/tangjames" target="_blank"><p>Github</p></a> / <a href="https://www.linkedin.com/in/tang-james/" target="_blank"><p>Linkedin</p></a> / <a href="mailto:tang.james91@gmail.com" target="_top"><p>Email</p></a>
 				</ul>
       </div>
     );
