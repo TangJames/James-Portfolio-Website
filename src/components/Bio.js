7a5c577ab731cf5bb63ad7bb249ec0ffbd998dcf
James Tang <ulrichyu@outie.net>
2017-12-01 10:29:54 -0600

    fixed bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,7 +7,7 @@ class Bio extends Component {
     return (
       <div className="Bio">
         <h2>A bit about me</h2>
-        <p>{"I've been around technology my whole life. As a Systems Administrator turned Programmer, I've fallen in love with coding. Focusing on backend, I'd like to say I'm involved with the entire web devlopement lifecycle."}</p>
+        <p>{"I've been around technology my whole life. As a Systems Administrator turned Programmer, I've fallen in love with coding. Focusing on minimalistic website design and backend, I'd like to say I'm involved with the entire web devlopement lifecycle."}</p>
 
         <p>I strive for clean and concicse code but not averse to getting the job done quick. My development skills include <b>HTML5, CSS3, JavaScript5/ES6, Python, AJAX/JSON, Bootstrap, SASS, jQuery, Node.JS, Express.JS, React.JS, React Native, MongoDB, Firebase, Oauth, noSQL, SQL, and Github.</b> This website was built in React!</p>
 
